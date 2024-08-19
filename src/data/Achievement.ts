62e2917bab88e30233d66a526b67355e6394c1bd
ashmi2001 <153133539+ashmi2001@users.noreply.github.com>
2024-08-19 18:04:52 -0400

    Added Achievements

---
+++ src/data/Achievement.ts
@@ -0,0 +1,14 @@
+const Achievement = [
+    {
+        title: "First place in Art competition",
+        description: ""
+    },
+    {
+        title: "First place in Art competition",
+        description: ""
+    },
+    {
+        title: "First place in Art competition",
+        description: ""
+    }
+]
